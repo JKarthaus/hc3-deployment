319b141ebba613fd84f3680197e5c2cc584c3f83
jkarthaus <joern.karthaus@gmail.com>
2019-12-27 19:07:51 +0100

    more Updates for production

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
+#!/bin/bash
+
+echo "-------------------------------------------------------"
+echo "INSTALLING HeatingControl 3 via Ansible on Raspberry Pi"
+echo "
+-------------------------------------------------------"
 
 
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' ansible|grep "install ok installed")
+echo Checking for ansible: $PKG_OK
+
+if [ "" == "$PKG_OK" ]; then
+  echo "No ansible installed. Setting up ansible."
+  sudo apt-get --force-yes --yes install ansible
+fi
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' cowsay|grep "install ok installed")
+echo Checking for cowsay: $PKG_OK
+
+if [ "" == "$PKG_OK" ]; then
+  echo "No cowsay installed. Setting up cowsay."
+  sudo apt-get --force-yes --yes install cowsay
+fi
 
 ## Insert this with your credentials in your.profile
 ## Do this if you want do build your own HeatingControl Version
